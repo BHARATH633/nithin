85853e148824e5524efd4fb59c5b9dc843aeffc1
BHARATH633 <bharathciramdasu@gmail.com>
2021-10-11 12:07:10 +0530

    added screenshot files

---
+++ src/pack/Screen.java
@@ -0,0 +1,35 @@
+package pack;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Screen {
+
+	public static void main(String[] args) throws IOException {
+
+		
+		WebDriver driver = new ChromeDriver();
+		//Actions ac = new Actions(driver);
+	//	driver.get("https://www.google.com");
+		
+	//File f  =  ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		
+//	   f  =	((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+//	System.out.println(f);
+		
+	//FileUtils.copyFile(f, new File("C:\\Users\\bc21223\\Desktop\\Selenium Files\\file1.jpg"));
+		
+
+
+		
+		
+	}
+
+}
